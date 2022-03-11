155090d5f9a5f3c1e280b38bca9a2b1b4a058ff4
monicaasl-tm <MS00833193@TechMahindra.com>
2022-03-11 15:53:52 -0800

    First commit

---
+++ src/test/java/tests/SeleniumDemo.java
@@ -0,0 +1,110 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class SeleniumDemo {
+	
+	public String baseURL = "https://demoqa.com";
+	String driverPath = "src/test/resources/chromedriver";
+	public ChromeDriver driver;
+	
+	@BeforeTest
+	private void startTesting() {
+		System.out.println("This is a Before Test");
+	}
+	
+	@BeforeMethod
+	private void startDriver(){
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.get(baseURL);
+	    driver.manage().window().maximize();
+	    System.out.println("Browser launched and maximized!");
+	}
+	
+	@Test 
+	public void sampleTestMethod() throws InterruptedException {
+		
+		Actions action = new Actions(driver);
+	    JavascriptExecutor executor = (JavascriptExecutor)driver;
+		
+        WebElement elementsSection = driver.findElement(By.xpath("//h5[text()='Elements']/../../../div"));
+		action.moveToElement(elementsSection).click().perform();
+		
+		WebElement buttonSection = driver.findElement(By.xpath("//span[text()='Buttons']/.."));
+		action.moveToElement(buttonSection).perform();
+		executor.executeScript("arguments[0].click();", buttonSection);
+		Thread.sleep(5000);
+		
+		WebElement singleClickButton = driver.findElement(By.xpath("//div[@class='mt-4'][2]//button"));
+		singleClickButton.click();
+		
+		WebElement dynamicMessage = driver.findElement(By.cssSelector("#dynamicClickMessage"));
+		Assert.assertEquals(dynamicMessage.getText(), "You have done a dynamic click");
+	}
+	
+	
+	//Homework
+	@Test 
+	public void sampleTestMethod2() throws InterruptedException {
+		
+		Actions action = new Actions(driver);
+	    JavascriptExecutor executor = (JavascriptExecutor)driver;
+		
+        WebElement elementsSection = driver.findElement(By.xpath("//h5[text()='Elements']/../../../div"));
+		action.moveToElement(elementsSection).click().perform();
+		
+		WebElement checkBoxSection = driver.findElement(By.xpath("//span[contains(text(),'Check Box')]"));
+		action.moveToElement(checkBoxSection).perform();
+		executor.executeScript("arguments[0].click();", checkBoxSection);
+		
+		WebElement checkBoxHome = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/ol[1]/li[1]/span[1]/label[1]/span[1]"));
+		checkBoxHome.click();
+		Thread.sleep(5000);
+		
+		WebElement messageResult = driver.findElement(By.xpath(" //span[contains(text(),'home')]"));
+		Assert.assertEquals(messageResult.getText(), "home");
+	}
+	
+	//Homework
+	@Test 
+	public void sampleTestMethod3() throws InterruptedException {
+		
+		Actions action = new Actions(driver);
+	    JavascriptExecutor executor = (JavascriptExecutor)driver;
+		
+        WebElement elementsSection = driver.findElement(By.xpath("//h5[text()='Elements']/../../../div"));
+		action.moveToElement(elementsSection).click().perform();
+		
+		WebElement linksSection = driver.findElement(By.cssSelector("#item-5"));
+		action.moveToElement(linksSection).perform();
+		executor.executeScript("arguments[0].click();", linksSection);
+		Thread.sleep(5000);
+		
+		WebElement noContentLink = driver.findElement(By.xpath("//a[@id='no-content']"));
+		noContentLink.click();
+		Thread.sleep(5000);
+		
+		WebElement linkResponse = driver.findElement(By.cssSelector("#linkResponse"));
+		Assert.assertEquals(linkResponse.getText(), "Link has responded with staus 204 and status text No Content");
+	}
+	
+	
+	@AfterTest
+	public void terminateBrowser() {
+		driver.close();
+	    System.out.println("Browser closed!");
+	}
+	
+	
+}
